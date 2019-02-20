da1bcac5a64a7b8375c79f6c6247e03f2c312c63
kzx <kjx336@163.com>
2019-02-20 15:12:53 +0800

    Signed-off-by: kzx <kjx336@163.com>

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,6 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
